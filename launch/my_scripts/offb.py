2c9f41e20879a3d69786329241e283958f0702cd
AshwinDisa <ashwin.disa@gmail.com>
2023-01-18 16:58:42 +0530

    main drone script

--- launch/my_scripts/offb.py
+++ launch/my_scripts/offb.py
@@ -2,9 +2,11 @@
 
 import rospy
 import math
+import numpy as np
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
 from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+import modern_robotics as mr
 
 class controller():
 
@@ -24,19 +26,19 @@ class controller():
 
         self.current_state = State()
 
-        rospy.Subscriber("mavros/state", State, self.state_callback)
+        rospy.Subscriber("uav0/mavros/state", State, self.state_callback)
 
-        rospy.Subscriber('mavros/local_position/pose',
+        rospy.Subscriber('uav0/mavros/local_position/pose',
                             PoseStamped, self.position_callback)
 
     def control(self):
 
         rate = rospy.Rate(20)
 
-        rospy.wait_for_service("/mavros/cmd/arming")
-        arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
-        rospy.wait_for_service("/mavros/set_mode")
-        set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+        rospy.wait_for_service("/uav0/mavros/cmd/arming")
+        arming_client = rospy.ServiceProxy("uav0/mavros/cmd/arming", CommandBool)
+        rospy.wait_for_service("/uav0/mavros/set_mode")
+        set_mode_client = rospy.ServiceProxy("uav0/mavros/set_mode", SetMode)
 
         offb_set_mode = SetModeRequest()
         offb_set_mode.custom_mode = 'OFFBOARD'
@@ -83,26 +85,8 @@ class controller():
 
                 self.flag = 1.0
                 circular_trajectory_mode.control()
+                # trajectory_mode.control()
                 rate.sleep()
-
-        # while not rospy.is_shutdown():
-
-        #     displacement = math.sqrt(pow((self.hover_x - self.current_x),2) +
-        #                                 pow((self.hover_y - self.current_y),2)
-        #                                 + pow((self.hover_z - self.current_z),2))
-
-        #     if (displacement > self.error):
-
-        #         hover_mode.publisher()
-        #         rate.sleep()
-
-        #     else:
-
-        #         while not rospy.is_shutdown():
-
-        #             # circular_trajectory_mode.control()
-        #             trajectory_mode.control()
-        #             rate.sleep()
 
     def state_callback(self, state_msg):
 
@@ -122,7 +106,7 @@ class hover():
         self.hover_y = hover_position[1]
         self.hover_z = hover_position[2]
 
-        self.pose_publisher = rospy.Publisher('mavros/setpoint_position/local',
+        self.pose_publisher = rospy.Publisher('uav0/mavros/setpoint_position/local',
                                     PoseStamped, queue_size = 10)
 
     def publisher(self):
@@ -149,7 +133,7 @@ class circular_trajectory():
         # self.desired_y = 0.0
         # self.desired_z = 0.0
 
-        self.pose_publisher = rospy.Publisher('mavros/setpoint_position/local',
+        self.pose_publisher = rospy.Publisher('uav0/mavros/setpoint_position/local',
                                     PoseStamped, queue_size = 10)
 
     def control(self):
@@ -158,11 +142,54 @@ class circular_trajectory():
 
         self.desired_x = self.xc + self.radius*math.cos(2*math.pi*t/self.time_of_rev)
         self.desired_y = self.yc + self.radius*math.sin(2*math.pi*t/self.time_of_rev)
-        self.desired_z = 2.0
+        self.desired_z = 5.0
 
         self.time += self.samplingTime
 
         self.publisher(self.desired_x, self.desired_y, self.desired_z)
+
+    def publisher(self, desired_x, desired_y, desired_z):
+
+        pose_msg = PoseStamped()
+
+        pose_msg.pose.position.x = desired_x
+        pose_msg.pose.position.y = desired_y
+        pose_msg.pose.position.z = desired_z
+
+        self.pose_publisher.publish(pose_msg)
+
+class straight_trajectory():
+
+    def __init__(self, trajectory, timegap, Xstart, Xend, samplingTime, Tf):
+
+        self.trajectory = trajectory
+        self.timegap = timegap
+        self.time = 0.0
+        self.Tf = Tf
+        self.count = 0
+        self.samplingTime = samplingTime
+
+        self.pose_publisher = rospy.Publisher('/uav0/mavros/setpoint_position/local',
+                                        PoseStamped, queue_size = 10)
+
+
+    def control(self):
+
+        pose = self.trajectory[self.count]
+
+        desired_x = pose[0][3]
+        desired_y = pose[1][3]
+        desired_z = pose[2][3]
+
+        desired_position = np.mat(np.array([desired_x, desired_y, desired_z]))
+
+        self.publisher(desired_x, desired_y, desired_z)
+
+        self.count += 1
+
+        if(self.count >= len(self.trajectory)):
+
+            rospy.signal_shutdown("completed")
 
     def publisher(self, desired_x, desired_y, desired_z):
 
@@ -180,13 +207,34 @@ if __name__ == "__main__":
 
         rospy.init_node("offb_node_py")
 
-        hover_position = [0, 0, 2]
+        hover_position = [0, 0, 5]
         hover_mode = hover(hover_position)
 
+        traj_initial_pos = np.array(hover_position)
+        traj_final_pos = np. array([8, 4, 6])
+
+        Xstart = np.zeros((4, 4))
+        Xend = np.zeros((4, 4))
+
+        Xstart[:3, 3] = traj_initial_pos
+        Xend[:3, 3] = traj_final_pos
+
+        # straight line trajectory
+        Tf = 8                                # time to reach from start to end position
+        samplingTime = 1/100                    # sampling time in seconds
+        N = int(Tf/samplingTime)                # number of samples
+        method = 5                              # interpolation method
+        trajectory = mr.CartesianTrajectory(
+                Xstart, Xend, Tf, N, method)    # get the trajectory
+        timegap = Tf / (N - 1.0)
+
         centre = [0, 0]
-        radius = 5.0
-        time_of_rev = 8.0
+        radius = 10.0
+        time_of_rev = 15.0
         samplingTime = 1/100
+
+        trajectory_mode = straight_trajectory(
+            trajectory, timegap, Xstart, Xend, samplingTime, Tf)
 
         circular_trajectory_mode = circular_trajectory(centre, radius,
                                         time_of_rev, samplingTime)
